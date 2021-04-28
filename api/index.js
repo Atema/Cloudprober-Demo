f3f4e9050a65cdd3d544537724367967570214af
Simon Persson <simon@akep.se>
2021-04-28 19:54:02 +0200

    added api

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+
+const app = new express();
+const start = new Date();
+
+app.get('/', (req, res) => {
+	const millis = Date.now() - start;
+	const seconds = Math.floor(millis / 1000);
+
+	if (seconds % 2 === 0) {
+		res.sendStatus(200);
+	} else {
+		res.sendStatus(404);
+	}
+});
+
+
+
+app.listen(3000, (err) => {
+	if (err) console.err(err);
+})
