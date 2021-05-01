67eaf38d022bd2177dede9ca23ac6b4cd4cad3df
Simon Persson <simon@akep.se>
2021-05-01 19:19:27 +0200

    added endpoint

--- testapi/index.js
+++ testapi/index.js
@@ -1,22 +1,30 @@
 const express = require('express');
 
-
 const app = new express();
 const start = new Date();
 
-app.get('/', (req, res) => {
-	const millis = Date.now() - start;
-	const minutes = Math.floor(millis / 60000);
+app.get('/drop', (req, res) => {
+  const millis = Date.now() - start;
+  const minutes = Math.floor(millis / 60000);
 
-	if (minutes % 2 === 0) {
-		res.sendStatus(200);
-	} else {
-		req.socket.end();
-	}
+  if (minutes % 2 === 0) {
+    res.sendStatus(200);
+  } else {
+    req.socket.end();
+  }
 });
 
+app.get('/error', (req, res) => {
+  const millis = Date.now() - start;
+  const minutes = Math.floor(millis / 60000);
 
+  if (minutes % 2 === 0) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(500);
+  }
+});
 
 app.listen(process.env.PORT || 3000, (err) => {
-	if (err) console.err(err);
-})
+  if (err) console.err(err);
+});
